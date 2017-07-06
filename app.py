780c2398e275290aa3d48dc10f8f32d67b4a46c7
15331094 <1617899539@qq.com>
2017-07-07 01:28:56 +0800

    add poster

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import *
 import arrange
+import random
 
 app = Flask(__name__)
 
@@ -7,9 +8,10 @@ info = arrange.get_product_info()
 
 @app.route("/")
 def home():
+    poster_num = random.randint(1, 4)
     Gradevins, Phonographs = info
     Gradevins = Gradevins[:4]
-    return render_template("home.html", Gradevins = Gradevins, Phonographs = Phonographs)
+    return render_template("home.html", Gradevins = Gradevins, Phonographs = Phonographs, poster_url = "/static/home/poster/" + str(poster_num) + ".jpg")
 
 
 @app.route("/phonographs")
@@ -37,14 +39,14 @@ def detail():
 
 @app.route("/list_search_content")
 def list_search_content():
-	search = request.args.get('search')
-	Gradevins, Phonographs = info
-	all_products = Gradevins + Phonographs
-	result_products = []
-	for product in all_products:
-		if product._name.find(search) != -1:
-			result_products.append(product)
-	return render_template("list.html", myproducts = result_products)
+    search = request.args.get('search')
+    Gradevins, Phonographs = info
+    all_products = Gradevins + Phonographs
+    result_products = []
+    for product in all_products:
+        if product._name.find(search) != -1:
+            result_products.append(product)
+    return render_template("list.html", myproducts = result_products)
 
 @app.route("/search", methods=['POST'])
 def search():
@@ -53,27 +55,28 @@ def search():
 
 @app.route("/contact")
 def contact():
-	info_id = int(request.args.get('info_id'))
-	style_id = [["pro1list1hh", "pro1list1"], ["pro1list1", "pro1list1hh"]]
-	content_id = [
-		[
-			"LOVPHONE ELECTRONIC CO., LTD.",
-			"Email: sales1@lovphone.com",
-			"Email:service@lovphone.com",
-			"Email: lovphone8@gmail.com",
-			"Skype : Lovphone",
-			"www.lovphone.com"
-		],
-		["Introduction"]		
-	]
-	#return render_template("contact.html")
-	if info_id == 0 or info_id == 1:
-		return render_template("contact.html", style_id = style_id[info_id], contact_information = content_id[info_id])
-	abort(401)
+    info_id = int(request.args.get('info_id'))
+    style_id = [["pro1list1hh", "pro1list1"], ["pro1list1", "pro1list1hh"]]
+    content_id = [
+        [
+            "LOVPHONE ELECTRONIC CO., LTD.",
+            "Email: sales1@lovphone.com",
+            "Email:service@lovphone.com",
+            "Email: lovphone8@gmail.com",
+            "Skype : Lovphone",
+            "www.lovphone.com"
+        ],
+        ["Introduction"]        
+    ]
+    #return render_template("contact.html")
+    if info_id == 0 or info_id == 1:
+        return render_template("contact.html", style_id = style_id[info_id], contact_information = content_id[info_id])
+    abort(401)
 
-		
+        
 
-app.run(host = "0.0.0.0", port = 80)
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 80)
 
 
 
