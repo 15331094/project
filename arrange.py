f354273eebd6f326aa74fa707799f07139309ffc
15331094 <1617899539@qq.com>
2017-07-13 00:47:10 +0800

    change ip to domain

--- arrange.py
+++ arrange.py
@@ -6,7 +6,7 @@ class product(object):
 	def __init__(self, name, img_url, description):
 		self._name = name
 		self._img_url = img_url
-		self._detailpageurl = "http://165.227.9.185:80/detail?name=" + name.replace(" ", "-")
+		self._detailpageurl = "http://www.ikdeer.com:80/detail?name=" + name.replace(" ", "-")
 		self._description = description
 	def get_name(self):
 		return self._name
@@ -18,7 +18,7 @@ class product(object):
 
 def get_product_info():
 	#酒柜
-	Gradevins_base_dir = "project/static/resources/Gradevin6.16"
+	Gradevins_base_dir = "/var/www/demoapp/project/static/resources/Gradevin6.16"
 	Gradevins = os.listdir(Gradevins_base_dir)
 	#酒柜和酒列表
 	Gradevins_list = []
@@ -27,13 +27,13 @@ def get_product_info():
 		imgs = os.listdir(Gradevins_base_dir + "/" + each_product)
 		pic_urls_list = []
 		for pic_url in imgs:
-			pic_urls_list.append(Gradevins_base_dir[8:] + "/" + each_product + "/" + pic_url)
+			pic_urls_list.append(Gradevins_base_dir[25:] + "/" + each_product + "/" + pic_url)
 		description = "Digital thermostat with push button controls an digital readout is easy to set and provides accurate temperature control Compact design fits in small spaces easily and requires only 4 of clearance on all sides! Interior LED light and clear glass door allows for optimal viewing Low noise level rating makes this unit a great option for any environment Thermoelectric design is vibration free to help preserve flavor and aroma Excellent customer reviews for superior temperature control Equipped with sliding and removable shelves for custom storage Easily stores up to 28 bottles of your favorite wine Modern design is made from top-quality materials and complements decor."
 		new_gradevins = product(name, pic_urls_list, description)
 		Gradevins_list.append(new_gradevins)
 
 	#留声机
-	Phonographs_base_dir = "project/static/resources/Phonograph6.16"
+	Phonographs_base_dir = "/var/www/demoapp/project/static/resources/Phonograph6.16"
 	Phonographs = os.listdir(Phonographs_base_dir)
 	#留声机6.16
 	Phonographs_list = []	
@@ -42,12 +42,12 @@ def get_product_info():
 		imgs = os.listdir(Phonographs_base_dir + "/" + each_product)
 		pic_urls_list = []
 		for pic_url in imgs:
-			pic_urls_list.append(Phonographs_base_dir[8:] + "/" + each_product + "/" + pic_url)
+			pic_urls_list.append(Phonographs_base_dir[25:] + "/" + each_product + "/" + pic_url)
 		description = ""
 		new_Phonographs = product(name, pic_urls_list, description)
 		Phonographs_list.append(new_Phonographs)
 	#留声机6.17
-	Phonographs_base_dir = "project/static/resources/Phonograph6.17"
+	Phonographs_base_dir = "/var/www/demoapp/project/static/resources/Phonograph6.17"
 	Phonographs = os.listdir(Phonographs_base_dir)
 
 	for each_product in Phonographs:
@@ -55,7 +55,7 @@ def get_product_info():
 		imgs = os.listdir(Phonographs_base_dir + "/" + each_product)
 		pic_urls_list = []
 		for pic_url in imgs:
-			pic_urls_list.append(Phonographs_base_dir[8:] + "/" + each_product + "/" + pic_url)
+			pic_urls_list.append(Phonographs_base_dir[25:] + "/" + each_product + "/" + pic_url)
 		description = ""
 		new_Phonographs = product(name, pic_urls_list, description)
 		Phonographs_list.append(new_Phonographs)
